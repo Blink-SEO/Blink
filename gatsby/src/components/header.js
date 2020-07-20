82558cb200e38e04b264bb4260973aa72b6eb422
Steven Bister <stevenbister@macbook-pro.home>
2020-07-20 15:09:02 +0100

    Set up header
    - Added query to allow gatsby image on logo
    - Added tailwind classes

--- gatsby/src/components/header.js
+++ gatsby/src/components/header.js
@@ -1,24 +1,33 @@
-import React from "react"
-import { Heading, Box, Grid } from "@chakra-ui/core"
-import { Link } from "gatsby"
-import GatsbyLogo from "../assets/svg/gatsby.inline.svg"
+import React from 'react'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
-export default () => (
-  <Heading as="h1">
-    <Link to="/">
-      <Grid gridTemplateColumns="50px 1fr" gridGap="20px">
-        <Box maxW={50}>
-          <GatsbyLogo />
-        </Box>
-        <span
-          style={{
-            transform: `translateY(5px)`,
-            display: `inline-block`,
-          }}
-        >
-          Gatsby Source WordPress V4 demo
-        </span>
-      </Grid>
-    </Link>
-  </Heading>
-)
+import Menu from './menu'
+
+const Header = () => {
+  const data = useStaticQuery(graphql`
+    {
+      file(relativePath: {eq: "BlinkLogo.png"}) {
+        childImageSharp {
+          fixed(width: 167) {
+            ...GatsbyImageSharpFixed_withWebp
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <header className='flex container p-10'>
+      <Link to='/' className='flex-initial max-w-xs'>
+        {/* Could use logo as an SVG */}
+        <Img fixed={ data.file.childImageSharp.fixed } />
+      </Link>
+
+      <Menu />
+    </header>
+  )
+}
+
+
+export default Header
