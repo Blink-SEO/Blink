2ebe6d4e06d260c014cb251af6025c052259b2ca
Steven Bister <stevenbister@macbook-pro.home>
2020-07-22 11:37:29 +0100

    Added custom font family

--- gatsby/tailwind.config.js
+++ gatsby/tailwind.config.js
@@ -1,6 +1,12 @@
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
   theme: {
+    fontFamily: {
+      sans: ['Gilmer Regular', ...defaultTheme.fontFamily.sans]
+    },
     container: {
       center: true,
     },
