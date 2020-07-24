bdbcd83ce132f7473b0882cd21cac1bbd0945f16
Steven Bister <stevenbister@macbook-pro.home>
2020-07-24 12:13:39 +0100

    Added cookie consent banner

--- gatsby/src/components/layout.js
+++ gatsby/src/components/layout.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import CookieConsent from 'react-cookie-consent';
+
 import Header from './header'
 import Footer from './footer'
 
@@ -11,6 +13,18 @@ const Layout = ({ children }) => (
       { children }
     </main>
     <Footer />
+
+    <CookieConsent
+      location="bottom"
+      buttonText="Accept"
+      enableDeclineButton
+      flipButtons
+      declineButtonText="Decline"
+      cookieName="gatsby-gdpr-google-tagmanager"
+      style={{ background: "#3f3f37" }}
+    >
+      We use cookies to ensure that we give you the best experience on our website.
+    </CookieConsent>
   </div>
 )
 
