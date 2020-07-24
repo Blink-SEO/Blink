5da4b276b0bc2a91ba530755ef340c8cac025925
Steven Bister <stevenbister@macbook-pro.home>
2020-07-24 17:21:39 +0100

    Removed imported animejs node module
    - It will be called in from the static folder instead

--- gatsby/src/components/template-parts/bar-chart.js
+++ gatsby/src/components/template-parts/bar-chart.js
@@ -1,40 +1,10 @@
 import React from 'react'
-import anime from 'animejs'
 
 import BarChartSVG from '../../assets/svgs/bar-chart.inline.svg'
 
 const BarChart = () => {
-
-  const tl = anime.timeline({
-    easing: "easeInOutCubic",
-    duration: 1500,
-  })
-  const offset = '-=100'
-
-  // IDs are clippaths within the imported svg
-  tl.add({
-    targets: "#barClipYellowThree",
-    translateY: [350, 0],
-  })
-  .add({
-    targets: "#barClipYellowTwo",
-    translateY: [450, 0],
-  }, offset)
-  .add({
-    targets: "#barClipYellowOne",
-    translateY: [550, 0],
-  }, offset)
-  .add({
-    targets: "#barClipRed",
-    translateY: [650, 0],
-  }, offset)
-  .add({
-    targets: "#SVGID_2_",
-    translateX: [-1600, 0],
-  })
-
   return (
-    <div className='overflow-visible'>
+    <div className='overflow-visible' style={{height: '80%'}}>
       <BarChartSVG className='w-full-1-5' />
     </div>
   )
