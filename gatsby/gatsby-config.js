ad5d75f252620a270df6a07d8bf84553524f433b
Steven Bister <stevenbister@macbook-pro.home>
2020-07-25 10:49:33 +0100

    Added site url to config to be used as the canonical url

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
   siteMetadata: {
     title: `SEO and Digital Marketing Agency | Search and Content | Blink SEO`,
     description: `Intelligent SEO and digital marketing from Blink, focused on delivering a real return on your investment. Call us on 01603 928247 to find out more.`,
+    siteUrl: `https://www.blinkseo.co.uk`, // No trailing slash allowed!
   },
   plugins: [
     `gatsby-plugin-sharp`,
