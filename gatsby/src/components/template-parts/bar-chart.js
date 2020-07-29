af947beed10186db34ef9a36af4622857e8897a7
Steven Bister <stevenbister@macbook-pro.home>
2020-07-29 16:38:15 +0100

    Added gsap as hook
    Use effect hook allows us to run the animation even after build

--- gatsby/src/components/template-parts/bar-chart.js
+++ gatsby/src/components/template-parts/bar-chart.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { TimelineLite } from "gsap/dist/gsap";
 
 import BarChartSVG from '../../assets/svgs/bar-chart.inline.svg'
 
 const BarChart = () => {
+  const tl = new TimelineLite()
+  const offset = '>-1'
+
+  useEffect(() => {
+    tl
+      .fromTo("#barClipYellowThree", {y: 650}, {y: 0, duration: 1})
+      .fromTo("#shadowThree", {y: -1800}, {y: 0, duration: 1}, offset)
+      .fromTo("#barClipYellowTwo", {y: 650}, {y: 0, duration: 1})
+      .fromTo("#shadowFour", {y: -1800}, {y: 0, duration: 1}, offset)
+      .fromTo("#barClipYellowOne", {y: 650}, {y: 0, duration: 1})
+      .fromTo("#shadowTwo", {y: -1800}, {y: 0, duration: 1}, offset)
+      .fromTo("#barClipRed", {y: 650}, {y: 0, duration: 1})
+      .fromTo("#shadowOne", {y: -1800}, {y: 0, duration: 1}, offset);
+  })
+
   return (
     <div className='overflow-visible' style={{height: '80%'}}>
-      <BarChartSVG className='w-full-1-5' />
+      <BarChartSVG className='w-full-1-5'  />
     </div>
   )
 }
