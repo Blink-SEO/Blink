0e481fa4f2f1be0fb1b30bb950888f6955bbc724
Steven Bister <stevenbister@macbook-pro.home>
2020-07-29 17:55:41 +0100

    Updated to use layout effect to fix blink on firefox
    - need to check on Chrome issue

--- gatsby/src/components/template-parts/bar-chart.js
+++ gatsby/src/components/template-parts/bar-chart.js
@@ -1,22 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useLayoutEffect } from 'react'
 import { TimelineLite } from "gsap/dist/gsap";
 
 import BarChartSVG from '../../assets/svgs/bar-chart.inline.svg'
 
 const BarChart = () => {
-  const tl = new TimelineLite()
-  const offset = '>-1'
 
-  useEffect(() => {
+  useLayoutEffect(() => {
+    const tl = new TimelineLite()
+    const offset = '>-1'
+
     tl
-      .fromTo("#barClipYellowThree", {y: 650}, {y: 0, duration: 1})
-      .fromTo("#shadowThree", {y: -1800}, {y: 0, duration: 1}, offset)
-      .fromTo("#barClipYellowTwo", {y: 650}, {y: 0, duration: 1})
-      .fromTo("#shadowFour", {y: -1800}, {y: 0, duration: 1}, offset)
-      .fromTo("#barClipYellowOne", {y: 650}, {y: 0, duration: 1})
-      .fromTo("#shadowTwo", {y: -1800}, {y: 0, duration: 1}, offset)
-      .fromTo("#barClipRed", {y: 650}, {y: 0, duration: 1})
-      .fromTo("#shadowOne", {y: -1800}, {y: 0, duration: 1}, offset);
+      .fromTo("#h", {y: 650}, {y: 0, duration: 1})
+      .fromTo("#c", {y: -1800}, {y: 0, duration: 1}, offset)
+      .fromTo("#g", {y: 650}, {y: 0, duration: 1})
+      .fromTo("#d", {y: -1800}, {y: 0, duration: 1}, offset)
+      .fromTo("#f", {y: 650}, {y: 0, duration: 1})
+      .fromTo("#b", {y: -1800}, {y: 0, duration: 1}, offset)
+      .fromTo("#e", {y: 650}, {y: 0, duration: 1})
+      .fromTo("#a", {y: -1800}, {y: 0, duration: 1}, offset);
   })
 
   return (
