2803369e061410d9a42d08ddb9acb74a7f0d1332
Steven Bister <stevenbister@macbook-pro.home>
2020-07-30 16:07:59 +0100

    Target ID for arrow
    - Will remove and change this once the rest of the front page starts to get made

--- gatsby/src/components/footer.js
+++ gatsby/src/components/footer.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 const Footer = () => {
 
   return (
-    <footer className='flex bg-grey py-10'>
+    <footer id='bottom' className='flex bg-grey py-10'>
       <div className="container px-6 sm:px-0">
         <p className='text-white'>&copy; { new Date().getFullYear() } BlinkSEO. All rights reserved.</p>
       </div>
