74aee3b4513bc810ef16d552af37c8f7d24c86c4
Steven Bister <stevenbister@macbook-pro.home>
2020-09-16 12:22:59 +0100

    Added: query for case studies

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -3,6 +3,8 @@ const { slash } = require(`gatsby-core-utils`)
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
+  const pageTemplate = path.resolve(`./src/templates/Page.js`)
+  const caseStudyTemplate = path.resolve(`./src/templates/single/Case-study.js`)
 
   // query content for WordPress pages
   const {
@@ -20,7 +22,21 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
-  const pageTemplate = path.resolve(`./src/templates/Page.js`)
+  // query content for Case Studies
+  const {
+    data: {
+      allWpCaseStudy: { nodes: allCaseStudies },
+    },
+  } = await graphql(`
+    query {
+      allWpCaseStudy(sort: {fields: date, order: ASC}) {
+        nodes {
+          id
+          slug
+        }
+      }
+    }
+  `)
 
   allPages.forEach(post => {
     createPage({
@@ -35,4 +51,20 @@ exports.createPages = async ({ graphql, actions }) => {
       },
     })
   })
+
+  allCaseStudies.forEach(post => {
+    createPage({
+      // will be the url for the page
+      path: `case-studies/${post.slug}`,
+      // specify the component template of your choice
+      component: slash(caseStudyTemplate),
+      // In the ^template's GraphQL query, 'id' will be available
+      // as a GraphQL variable to query for this post's data.
+      context: {
+        id: post.id,
+      },
+    })
+  })
+
+
 }
