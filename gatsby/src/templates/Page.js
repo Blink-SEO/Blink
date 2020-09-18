0910652638429810d46f44c1da5f1948d79fc0fc
Steven Bister <stevenbister@macbook-pro.home>
2020-09-18 16:42:23 +0100

    Moved query above component

--- gatsby/src/templates/Page.js
+++ gatsby/src/templates/Page.js
@@ -6,33 +6,6 @@ import Layout from '../components/layout'
 import Hero from '../components/template-parts/PageHero'
 import CaseStudiesLoop from '../components/template-parts/Loop-case-studies'
 
-export default ({ data }) => {
-  const { title, content, pageSettings, featuredImage, seo, template } = data.page
-
-  return (
-    <Layout backgroundColor={ pageSettings.backgroundColour } className='page' >
-      <SEO
-        title={ seo.title }
-        description={ seo.metaDesc }
-        // image={ featuredImage.node.sourceUrl }
-        ogAuthor={ seo.opengraphAuthor }
-        ogDescription={ seo.opengraphDescription }
-        ogTitle={ seo.opengraphTitle }
-        // ogImage={ seo.opengraphImage.sourceUrl }
-      />
-
-      <Hero title={ title } subtitle={ pageSettings.subtitle } />
-
-      <article className="[ flow ] [ relative ]">
-        { content && <section className='[ entry-content flow ]' dangerouslySetInnerHTML={{ __html: content }} /> }
-
-        { template.templateName === 'Case Studies' && <CaseStudiesLoop /> }
-      </article>
-
-    </Layout>
-  )
-}
-
 export const query = graphql`
   query page($id: String!) {
     page: wpPage(id: { eq: $id }) {
@@ -69,3 +42,30 @@ export const query = graphql`
     }
   }
 `
+
+export default ({ data }) => {
+  const { title, content, pageSettings, featuredImage, seo, template } = data.page
+
+  return (
+    <Layout backgroundColor={ pageSettings.backgroundColour } className='page' >
+      <SEO
+        title={ seo.title }
+        description={ seo.metaDesc }
+        // image={ featuredImage.node.sourceUrl }
+        ogAuthor={ seo.opengraphAuthor }
+        ogDescription={ seo.opengraphDescription }
+        ogTitle={ seo.opengraphTitle }
+        // ogImage={ seo.opengraphImage.sourceUrl }
+      />
+
+      <Hero title={ title } subtitle={ pageSettings.subtitle } className='hero--full' />
+
+      <article className="[ flow ] [ relative ]">
+        { content && <section className='[ entry-content flow ]' dangerouslySetInnerHTML={{ __html: content }} /> }
+
+        { template.templateName === 'Case Studies' && <CaseStudiesLoop /> }
+      </article>
+
+    </Layout>
+  )
+}
