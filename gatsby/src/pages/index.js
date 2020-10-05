c18d867329203bc04d94da1d5e349bfbc414d173
Steven Bister <stevenbister@macbook-pro.home>
2020-10-05 15:21:31 +0100

    Created styles for homepage title

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -58,19 +58,17 @@ export default ({ data }) => {
         ogTitle={ seo.opengraphTitle }
         ogImage={ seo.opengraphImage.localFile.childImageSharp.fluid.src }
       />
-      {/* TODO: Make this a hero component? */}
-      <article className='grid grid-flow-row sm:grid-flow-col sm:grid-cols-2 gap-16 min-h-screen mx-auto'>
-        <div className='border-l-2 border-dark-yellow pl-8 mb-8'>
-          <h1 className='hero-title text-white text-4xl sm:text-5xl lg:text-6xl leading-tight mb-5'>{ title }</h1>
-          { content && <section className='hero-section max-w-45ch' dangerouslySetInnerHTML={{ __html: content }} /> }
-          <div className='max-w-45ch'>
-            <ConvertKitFrom />
+      <article>
+        <section className='[ grid grid-flow-row sm:grid-flow-col sm:grid-cols-2 gap-16 ] [ mx-auto ]'>
+          <div className='[ pl-8 mb-8 ]'>
+            <h1 className='[ hero-title hero-title--home ] [ text-white text-4xl sm:text-5xl lg:text-6xl leading-tight mb-5 ]'>{ title }<span className="border-block"></span></h1>
+            { content && <div className='[ hero-section ] [ max-w-45ch ]' dangerouslySetInnerHTML={{ __html: content }} /> }
           </div>
-        </div>
 
-        <div className='overflow-visible relative'>
-          <BarChart duration={ 1.3 } />
-        </div>
+          <div className='[ overflow-visible ] [ relative ]'>
+            <BarChart duration={ 1.3 } />
+          </div>
+        </section>
       </article>
     </Layout>
   )
