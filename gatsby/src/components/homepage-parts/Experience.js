2dd375e2eee0c4d379bc8589eac528bea3b33dbc
Steven Bister <stevenbister@macbook-pro.home>
2020-10-06 15:32:36 +0100

    Added block to contain experience details to show on the frontpage

--- gatsby/src/components/homepage-parts/Experience.js
+++ gatsby/src/components/homepage-parts/Experience.js
@@ -18,26 +18,46 @@ const Experience = () => {
                 }
               }
             }
+            resultsCallout {
+              linkTo {
+                ... on WpCaseStudy {
+                  uri
+                  title
+                }
+              }
+              results
+            }
           }
         }
       }
     }
   `)
 
-  const { clients } = data.wpPage.homepage.experience
+  const { clients, resultsCallout } = data.wpPage.homepage.experience
 
   return (
     <section className="[ full-bleed ] [ py-16 ] [ bg-red ]">
-      <div className="[ flow ] [ container ]">
+      <div className="[ flow ] [ container ] [ px-6 sm:px-0 ]">
         <h2 className="leading-normal">Our experience</h2>
 
         <div className="[ grid grid-cols-2 md:grid-cols-5 lg:grid-cols-6 gap-6 ]">
         { clients.map( (client, i) => (
+
           client?.clientLogo?.remoteFile?.childImageSharp?.fluid && <Img fluid={ client.clientLogo.remoteFile.childImageSharp.fluid } alt={ client.clientLogo.altText } key={i} className="h-full" />
+
         )) }
         </div>
 
-        <h2>Our results</h2>
+        <h2 className="leading-normal">Our results</h2>
+        <div className="[ grid grid-cols-2 md:grid-cols-5 lg:grid-cols-7 gap-6 ]">
+          {resultsCallout.map( (result, i) => (
+
+            <Link key={i} to={ normalizePath(result.linkTo.uri) } className="[ col-span-2 ] [ text-white no-underline ] [ bg-black bg-opacity-25 ]">
+              <div className="[ callout ]" dangerouslySetInnerHTML={{ __html: result.results }} />
+            </Link>
+
+          )) }
+        </div>
       </div>
     </section>
   )
