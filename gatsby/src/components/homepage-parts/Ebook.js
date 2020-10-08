ffb6e13b4eed6ebfbff3ff283379023f9371be84
Steven Bister <stevenbister@macbook-pro.home>
2020-10-08 12:50:58 +0100

    Created a wrapper for homepage elements
    A full bleed wrapper that'll help to bootstrap out the full bleed on homepage element

--- gatsby/src/components/homepage-parts/Ebook.js
+++ gatsby/src/components/homepage-parts/Ebook.js
@@ -2,6 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 import ConvertKitFrom from "../template-parts/ConvertKitForm"
+import Wrapper from "./HomeSectionWrapper"
 
 const Ebook = () => {
   const data = useStaticQuery(graphql`
@@ -20,23 +21,22 @@ const Ebook = () => {
   const { ebook } = data.wpPage.homepage
 
   return (
-    <section className="[ full-bleed ] [ py-16 ] [ bg-teal ]">
-      <div className="[ flow ] [ container ] [ px-6 sm:px-0 ]">
-        <div className="[ grid grid-cols-8 col-gap-10 lg:row-gap-16 ] [ relative ]">
+    <Wrapper className="[ bg-teal ]">
+      <div className="[ grid grid-cols-8 col-gap-10 lg:row-gap-16 ] [ relative ]">
 
-          <h2 className="[ row-start-1 col-start-1 col-end-7 ]">Download our free</h2>
-          <span className="[ h2 rotate rotate--center rotate--mob-none ] [ row-start-2 ] [ lg:absolute lg:bottom-0 ] [ lg:text-10xl ]" style={{ margin: '0' }}>eBook</span>
-          <div className="[ row-start-3 md:row-start-3 lg:row-start-2 col-start-1 col-end-9 lg:col-start-2 md:col-end-5 ] [ text-white ]" dangerouslySetInnerHTML={{ __html: ebook.content }} />
+        <h2 className="[ row-start-1 col-start-1 col-end-7 ]">Download our free</h2>
+        <span className="[ h2 rotate rotate--center rotate--mob-none ] [ row-start-2 ] [ lg:absolute lg:bottom-0 ] [ lg:text-10xl ]" style={{ margin: '0' }}>eBook</span>
 
-          <div className="[ row-start-4 md:row-start-3 lg:row-start-2 col-start-1 col-end-9 md:col-start-5 lg:col-end-8 ] [ text-2xl text-white ]">
+        <div className="[ row-start-3 md:row-start-3 lg:row-start-2 col-start-1 col-end-9 lg:col-start-2 md:col-end-5 ] [ text-white ]" dangerouslySetInnerHTML={{ __html: ebook.content }} />
 
-            <div dangerouslySetInnerHTML={{ __html: ebook.formIntro }} />
-            <ConvertKitFrom />
+        <div className="[ row-start-4 md:row-start-3 lg:row-start-2 col-start-1 col-end-9 md:col-start-5 lg:col-end-8 ] [ text-2xl text-white ]">
 
-          </div>
+          <div dangerouslySetInnerHTML={{ __html: ebook.formIntro }} />
+          <ConvertKitFrom />
+
         </div>
       </div>
-    </section>
+    </Wrapper>
   )
 }
 
--- gatsby/src/components/homepage-parts/Experience.js
+++ gatsby/src/components/homepage-parts/Experience.js
@@ -3,6 +3,7 @@ import { Link, useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 
 import { normalizePath } from "../../utils/get-url-path"
+import Wrapper from "./HomeSectionWrapper"
 
 const Experience = () => {
   const data = useStaticQuery(graphql`
@@ -36,30 +37,28 @@ const Experience = () => {
   const { clients, resultsCallout } = data.wpPage.homepage.experience
 
   return (
-    <section className="[ full-bleed ] [ py-16 ] [ bg-red ]">
-      <div className="[ flow ] [ container ] [ px-6 sm:px-0 ]">
-        <h2 className="leading-normal">Our experience</h2>
+    <Wrapper className="[ bg-red ]">
+      <h2 className="leading-normal">Our experience</h2>
 
-        <div className="[ grid grid-cols-2 md:grid-cols-5 lg:grid-cols-6 gap-6 ]">
-        { clients.map( (client, i) => (
+      <div className="[ grid grid-cols-2 md:grid-cols-5 lg:grid-cols-6 gap-6 ]">
+      { clients.map( (client, i) => (
 
-          client?.clientLogo?.remoteFile?.childImageSharp?.fluid && <Img fluid={ client.clientLogo.remoteFile.childImageSharp.fluid } alt={ client.clientLogo.altText } key={i} className="h-full" />
+        client?.clientLogo?.remoteFile?.childImageSharp?.fluid && <Img fluid={ client.clientLogo.remoteFile.childImageSharp.fluid } alt={ client.clientLogo.altText } key={i} className="h-full" />
+
+      )) }
+      </div>
+
+      <h2 className="leading-normal">Our results</h2>
+      <div className="[ grid grid-cols-2 md:grid-cols-5 lg:grid-cols-7 gap-6 ]">
+        {resultsCallout.map( (result, i) => (
+
+          <Link key={i} to={ normalizePath(result.linkTo.uri) } className="[ col-span-2 ] [ text-white no-underline ] [ bg-black bg-opacity-25 ]">
+            <div className="[ callout ]" dangerouslySetInnerHTML={{ __html: result.results }} />
+          </Link>
 
         )) }
-        </div>
-
-        <h2 className="leading-normal">Our results</h2>
-        <div className="[ grid grid-cols-2 md:grid-cols-5 lg:grid-cols-7 gap-6 ]">
-          {resultsCallout.map( (result, i) => (
-
-            <Link key={i} to={ normalizePath(result.linkTo.uri) } className="[ col-span-2 ] [ text-white no-underline ] [ bg-black bg-opacity-25 ]">
-              <div className="[ callout ]" dangerouslySetInnerHTML={{ __html: result.results }} />
-            </Link>
-
-          )) }
-        </div>
       </div>
-    </section>
+    </Wrapper>
   )
 }
 
