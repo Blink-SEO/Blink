9eb4288b9a5357d0b41836c6de0d03485cc9a604
Steven Bister <stevenbister@macbook-pro.home>
2020-10-08 16:35:11 +0100

    Added flex style to wrapper
    This will help us force the content into the center with justify

--- gatsby/src/components/homepage-parts/HomeSectionWrapper.js
+++ gatsby/src/components/homepage-parts/HomeSectionWrapper.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Wrapper = ({ children, className }) => (
-  <section className={`[ full-bleed ] [ py-16 ] ${className}`}>
+  <section className={`[ full-bleed ] [ flex flex-col ] [ justify-center ] [ py-16 ] [ min-h-screen ] ${className}`}>
     <div className="[ flow ] [ container ] [ px-6 sm:px-0 ]">
       { children }
     </div>
