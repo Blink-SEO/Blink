f85440e64c2ed7fac0cb6c8ce2e8d6b50b51afdc
Steven Bister <stevenbister@macbook-pro.home>
2020-10-19 10:54:35 +0100

    Restore holdingpage homepage markup

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,21 +1,10 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-import SEO from "../components/seo"
+import SEO from '../components/seo'
 import Layout from "../components/layout"
-import Contact from "../components/contactArea"
-
-import BarChart from "../components/template-parts/bar-chart"
-import Arrow from "../components/Img/DownArrow-Bounce"
-
-import Experience from "../components/homepage-parts/Experience"
-import Ebook from "../components/homepage-parts/Ebook"
-import About from "../components/homepage-parts/About"
-import Services from "../components/homepage-parts/Services"
-import WordCloud from "../components/homepage-parts/WordCloud"
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import ConvertKitFrom from '../components/template-parts/ConvertKitForm'
+import BarChart from '../components/template-parts/bar-chart'
 
 export const query = graphql`
 {
@@ -34,10 +23,6 @@ export const query = graphql`
           }
         }
       }
-    }
-    contactBlock {
-      title
-      message
     }
     seo {
       title
@@ -60,7 +45,7 @@ export const query = graphql`
 `
 
 export default ({ data }) => {
-  const { title, content, featuredImage, contactBlock, seo } = data.wpPage
+  const { title, content, featuredImage, seo } = data.wpPage
 
   return (
     <Layout>
@@ -73,33 +58,19 @@ export default ({ data }) => {
         ogTitle={ seo.opengraphTitle }
         ogImage={ seo.opengraphImage.localFile.childImageSharp.fluid.src }
       />
-      <article>
-        <section className='[ grid grid-flow-row sm:grid-flow-col sm:grid-cols-6 md:gap-8 ] [ mx-auto ] [ relative ]'>
-          <div className='[ row-start-1 col-start-1 col-end-7 lg:col-end-4 ] [ mb-8 ]'>
-            <h1 className='[ hero-title hero-title--home ] [ text-white text-4xl sm:text-5xl lg:text-6xl leading-tight mb-5 ]'>{ title }<span className="border-block"></span></h1>
-            { content && <div className='[ hero-section ] [ max-w-45ch ]' dangerouslySetInnerHTML={{ __html: content }} /> }
+      {/* TODO: Make this a hero component? */}
+      <article className='grid grid-flow-row sm:grid-flow-col sm:grid-cols-2 gap-16 min-h-screen mx-auto'>
+        <div className='border-l-2 border-dark-yellow pl-8 mb-8'>
+          <h1 className='hero-title text-white text-4xl sm:text-5xl lg:text-6xl leading-tight mb-5'>{ title }</h1>
+          { content && <section className='hero-section max-w-45ch' dangerouslySetInnerHTML={{ __html: content }} /> }
+          <div className='max-w-45ch'>
+            <ConvertKitFrom />
           </div>
+        </div>
 
-          <Arrow className="[ row-start-2 md:row-start-1 col-start-1 ] [ md:absolute md:right-50 bottom-25 ]" />
-
-          <div className='[ row-start-2 md:row-start-1 col-start-4 col-end-7 ] [ overflow-visible ] [ relative ]'>
-            <BarChart duration={ 1.3 } />
-          </div>
-
-          <FontAwesomeIcon icon={ faPlus } size="2x" className="[ row-start-1 col-start-1 ] [ self-end ] [ lg:mb-6 ] [ text-white ] [ hidden md:block ]" />
-        </section>
-
-        <Experience />
-
-        <Ebook />
-
-        <About />
-
-        <Services />
-
-        <WordCloud />
-
-        { contactBlock && <Contact backgroundColor="bg-dark-red" title={ contactBlock.title } message={ contactBlock.message } />}
+        <div className='overflow-visible relative'>
+          <BarChart duration={ 1.3 } />
+        </div>
       </article>
     </Layout>
   )
