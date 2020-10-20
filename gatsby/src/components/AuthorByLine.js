3f4ccb8af2963b2e19ce64ea5ea19bcd48516774
Steven Bister <stevenbister@macbook-pro.home>
2020-10-20 12:04:47 +0100

    Update: Author to custom field so we can have images

--- gatsby/src/components/AuthorByLine.js
+++ gatsby/src/components/AuthorByLine.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Img from 'gatsby-image'
 import PropTypes from 'prop-types'
 
 
 const ByLine = ({ author, title, avatar, readTime, className }) => (
   <div className={`[ byline ${className} ] [ flex items-center ] [ max-w-xs ]`}>
-    { avatar && <img src={`${avatar}`} alt="" className="[ mr-4 ] [ rounded-full ]" /> }
+    { avatar && <Img fluid={avatar.node.remoteFile.childImageSharp.fluid } alt="" className="[ mr-4 ] [ w-24 h-24 ] [ rounded-full ]" />}
+
     <div>
       { author && <p>by { author }</p> }
       { title && <p>{ title }</p> }
