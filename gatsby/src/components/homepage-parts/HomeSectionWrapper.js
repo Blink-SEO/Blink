0905910aebcb04cf500e12884fa7fc810a12f570
Steven Bister <stevenbister@macbook-pro.home>
2020-11-02 17:10:05 +0000

    Added proptype for image

--- gatsby/src/components/homepage-parts/HomeSectionWrapper.js
+++ gatsby/src/components/homepage-parts/HomeSectionWrapper.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Wrapper = ({ children, className, style }) => (
-  <section className={`[ full-bleed ] [ flex flex-col ] [ py-16 ] ${className}`} style={style}>
+const Wrapper = ({ children, className, style }) => {
+  console.log(style);
+  return (<section className={`[ full-bleed ] [ flex flex-col ] [ py-16 ] ${className}`} style={style}>
     <div className="[ flow flow--small ] [ container ] [ px-6 sm:px-0 ]">
       { children }
     </div>
-  </section>
-)
+  </section>)
+}
+
 
 Wrapper.propTypes = {
   className: PropTypes.string,
+  style: PropTypes.object,
 }
 
 export default Wrapper
