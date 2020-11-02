f70ab1b4ea3aa48a8e5359057ff2f324106e3696
Steven Bister <stevenbister@macbook-pro.home>
2020-11-02 17:03:34 +0000

    Added style prop for passing inline styles down

--- gatsby/src/components/homepage-parts/HomeSectionWrapper.js
+++ gatsby/src/components/homepage-parts/HomeSectionWrapper.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Wrapper = ({ children, className }) => (
-  <section className={`[ full-bleed ] [ flex flex-col ] [ py-16 ] ${className}`}>
+const Wrapper = ({ children, className, style }) => (
+  <section className={`[ full-bleed ] [ flex flex-col ] [ py-16 ] ${className}`} style={style}>
     <div className="[ flow flow--small ] [ container ] [ px-6 sm:px-0 ]">
       { children }
     </div>
