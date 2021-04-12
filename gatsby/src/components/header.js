a16d44a91c3906ef2a97b532b7fed3e1bfd41c8d
Steven Bister <stevebister@gmail.com>
2021-04-12 16:22:49 +0100

    Feat: Added check for whether banner is set to display

--- gatsby/src/components/header.js
+++ gatsby/src/components/header.js
@@ -5,46 +5,74 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhone, faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 import Menu from './menu'
+import WebinarBanner from './template-parts/WebinarBanner'
 
 const Header = () => {
-  const [ Active, isActive ] = useState( false );
+  const [Active, isActive] = useState(false)
 
   const data = useStaticQuery(graphql`
     {
-      file(relativePath: {eq: "BlinkLogo.png"}) {
+      file(relativePath: { eq: "BlinkLogo.png" }) {
         childImageSharp {
           fixed(width: 167) {
             ...GatsbyImageSharpFixed_withWebp_noBase64
           }
         }
       }
+      wp {
+        webinar {
+          webinarSettings {
+            webinarBanner {
+              displayBanner
+            }
+          }
+        }
+      }
     }
   `)
 
+  const { displayBanner } = data.wp.webinar.webinarSettings.webinarBanner
+
   return (
-    <header className="[ flex justify-between items-center ] [ container relative ] [ px-6 sm:px-0 pt-10 pb-20 ]">
-      <Link to="/" className="flex-initial max-w-xs">
-        <Img fixed={ data.file.childImageSharp.fixed } fadeIn={ false } loading="eager" alt="Blink SEO" />
-      </Link>
+    <>
+      {displayBanner && <WebinarBanner />}
+      <header className="[ flex justify-between items-center ] [ container relative ] [ px-6 sm:px-0 pt-10 pb-20 ]">
+        <Link to="/" className="flex-initial max-w-xs">
+          <Img
+            fixed={data.file.childImageSharp.fixed}
+            fadeIn={false}
+            loading="eager"
+            alt="Blink SEO"
+          />
+        </Link>
 
-      <div className="[ menu__container menu__container--active ]" data-active={ Active }>
-        <button className="menu-button menu-button--close" onClick={ () => isActive( false ) }>
-          <FontAwesomeIcon icon={ faTimes } size="3x" />
-        </button>
+        <div
+          className="[ menu__container menu__container--active ]"
+          data-active={Active}
+        >
+          <button
+            className="menu-button menu-button--close"
+            onClick={() => isActive(false)}
+          >
+            <FontAwesomeIcon icon={faTimes} size="3x" />
+          </button>
 
-        <Menu />
+          <Menu />
 
-        <div className="[ menu__item menu__item--large menu__item--large ] [ self-center ]">
-          <a href="tel:+441603928247"> <FontAwesomeIcon icon={ faPhone } size="sm" /> 01603 928247</a>
+          <div className="[ menu__item menu__item--large menu__item--large ] [ self-center ]">
+            <a href="tel:+441603928247">
+              {' '}
+              <FontAwesomeIcon icon={faPhone} size="sm" /> 01603 928247
+            </a>
+          </div>
         </div>
-      </div>
 
-      <button className="menu-button" onClick={ () => isActive( true ) }>
-        <FontAwesomeIcon icon={ faBars } size="3x" />
-      </button>
-    </header>
+        <button className="menu-button" onClick={() => isActive(true)}>
+          <FontAwesomeIcon icon={faBars} size="3x" />
+        </button>
+      </header>
+    </>
   )
 }
-
 
 export default Header
