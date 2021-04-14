49364f221d3b9a7c4d9a80019e81997ed63b5324
Steven Bister <stevebister@gmail.com>
2021-04-14 11:37:51 +0100

    Fix: left graphql query behind

--- gatsby/src/components/template-parts/WebinarUpcoming.js
+++ gatsby/src/components/template-parts/WebinarUpcoming.js
@@ -12,7 +12,6 @@ const WebinarUpcoming = () => {
               noEventsFallbackHeader
               noEventsFallbackText
               upcomingEventsHeading
-              upcomingEventLongDescription
             }
           }
         }
