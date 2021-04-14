c48ec875a59d50ca43c8b0bda1e3ad54dfd3e653
Steven Bister <stevebister@gmail.com>
2021-04-14 10:29:58 +0100

    Feat: Update eventbrite to clickmeeting api

--- gatsby/src/components/template-parts/WebinarUpcoming.js
+++ gatsby/src/components/template-parts/WebinarUpcoming.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
-import EventbriteButton from 'react-eventbrite-popup-checkout'
-import Readmore from './Accordion'
 
 const WebinarUpcoming = () => {
   const data = useStaticQuery(graphql`
@@ -11,7 +9,6 @@ const WebinarUpcoming = () => {
           webinarSettings {
             upcomingWebinars {
               webinarApiKey
-              eventbriteOrganisationId
               noEventsFallbackHeader
               noEventsFallbackText
               upcomingEventsHeading
@@ -24,36 +21,27 @@ const WebinarUpcoming = () => {
   `)
 
   const {
-    eventbriteOrganisationId,
     webinarApiKey,
     noEventsFallbackHeader,
     noEventsFallbackText,
     upcomingEventsHeading,
-    upcomingEventLongDescription,
   } = data.wp.webinar.webinarSettings.upcomingWebinars
 
   const [error, setError] = useState(null)
   const [isLoaded, setIsLoaded] = useState(false)
   const [events, setEvents] = useState([])
   useEffect(() => {
-    // get data from eventbrite api
-    fetch(
-      `https://www.eventbriteapi.com/v3/organizations/${eventbriteOrganisationId}/events/`,
-      // TODO: Set this param when we're ready to go live so we aren't showing unpublished events to the public
-      // ?status=live&time_filter=current_future
-      {
-        headers: {
-          Authorization: `Bearer ${webinarApiKey}`,
-          'Content-Type': 'application/x-www-form-urlencoded',
-          Accept: '*/*',
-        },
-      }
-    )
+    // get data from clickmeeting api
+    fetch(`https://api.clickmeeting.com/v1/conferences/active`, {
+      headers: {
+        'X-Api-Key': webinarApiKey,
+      },
+    })
       .then((response) => response.json()) // parse JSON from request
       .then(
         (result) => {
           setIsLoaded(true)
-          setEvents(result.events) // pass result to items
+          setEvents(result) // pass result to items
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
@@ -63,7 +51,7 @@ const WebinarUpcoming = () => {
           setError(error)
         }
       )
-  }, [eventbriteOrganisationId, webinarApiKey])
+  }, [webinarApiKey])
 
   let card
   if (events.length > 0) {
@@ -73,32 +61,42 @@ const WebinarUpcoming = () => {
       month: 'long',
       day: 'numeric',
     }
-    card = Object.entries(events).map(([key, value]) => (
-      <li
-        key={key + value.id}
-        className="[ card card--webinar flow ] [ col-start-1 col-end-5 ] [ bg-black ]"
-      >
-        <h3 className="text-3xl">{value.name.text}</h3>
-        <p className="text-xl">
-          {new Date(value.start.local).toLocaleDateString(undefined, options)}
-        </p>
-        <p>{value.description.text}</p>
+    card = Object.entries(events).map(([key, value]) => {
+      const date = new Date(value.starts_at).toLocaleDateString(
+        undefined,
+        options
+      )
+      const time = new Date(value.starts_at).toLocaleTimeString('en-GB', {
+        hour: 'numeric',
+        hour12: true,
+      })
 
-        <Readmore
-          buttonText="Read more"
-          content={upcomingEventLongDescription}
-        />
+      return (
+        <li
+          key={key + value.id}
+          className="[ card card--webinar flow ] [ col-start-1 col-end-5 ] [ bg-black ]"
+        >
+          <h3 className="text-3xl">{value.name}</h3>
+          <p className="text-xl">
+            {date}, {time}
+          </p>
 
-        <div className="[ grid sm:grid-flow-col grid-cols-3 ]">
-          <EventbriteButton
-            ebEventId={value.id}
-            className="[ col-start-1 col-end-2 ] [ py-3 mt-3 ] [ bg-white text-black ]"
-          >
-            Register
-          </EventbriteButton>
-        </div>
-      </li>
-    ))
+          <div
+            className="flow"
+            dangerouslySetInnerHTML={{ __html: value.lobby_description }}
+          />
+
+          <div className="[ grid sm:grid-flow-col grid-cols-3 ]">
+            <a
+              href={value.room_url}
+              className="[ col-start-1 col-end-2 ] [ py-3 mt-3 ] [ bg-white text-black text-center ]"
+            >
+              Register
+            </a>
+          </div>
+        </li>
+      )
+    })
   } else {
     card = (
       <li className="[ card card--webinar flow ] [ col-start-1 col-end-5 ] [ bg-black ]">
